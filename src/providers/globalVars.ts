cdd3d41585c5e3f469be6ffcf93d30e09469972a
Anisur <anisur.mullick@gmail.com>
2017-01-14 14:02:11 +0530

    Payment update functionality added

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -2,13 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalVars {
-  
-  /*
-  public authURL: string = "http://billing.pa247.net/index.php?id=34";
-  public logoutURL: string = "http://billing.pa247.net/routing/logout/";
-  public customerURL: string = "http://billing.pa247.net/routing/customer/";
-  public billURL: string = "http://billing.pa247.net/routing/customer-bill/";
- */
+ 
 
   public authURL: string = "http://billing.local/index.php?id=29";
   public logoutURL: string = "http://billing.local/routing/logout/";
